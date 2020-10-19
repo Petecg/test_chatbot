b60bc173a0831afa3aca4bbdf71aaef216605ec8
Pete CG <62259471+Petecg@users.noreply.github.com>
2020-10-19 18:24:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 // Imports dependencies and set up http server
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express().use(bodyParser.json()); // creates express http server
+const express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 // Sets server port and logs message on success
-const app.listen(process.env.PORT || 5000;, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 5000;, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => { 
