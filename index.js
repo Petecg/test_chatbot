c1ba0051a341dfe75249af8a84bbc2c3961fb66e
Pete CG <62259471+Petecg@users.noreply.github.com>
2020-10-20 11:21:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.listen(process.env.PORT || 5000, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook
 app.post('/webhook', (req, res) => {
+  let fbJson=JSON.stringify(req.body);
+  console.log(fbJson);
   let body = req.body;
   if (body.object === 'page') { // Checks this is an event from a page subscription
     body.entry.forEach(function(entry) { // Iterates over each entry - there may be multiple if batched
