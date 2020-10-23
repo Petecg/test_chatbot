005ca804875664c8867b73a526d111cfedc7ce23
Pete CG <62259471+Petecg@users.noreply.github.com>
2020-10-23 13:33:42 -0500

    Handover test (#4)
    * Handover test 01
    
    * Handover test 02
    
    * Handover test 03 - Procesamiento de evento standby
    
    * Handover test 02.1 - Revision tipo de objeto
    
    * Handover test 3.0 - Proceso de varios eventos
    
    * Handover test 03.1 - Corrección error de dedo en evento Standby

--- index.js
+++ index.js
@@ -17,17 +17,23 @@ app.post('/webhook', (req, res) => {
   let body = req.body;
   if (body.object === 'page') { // Checks this is an event from a page subscription
     body.entry.forEach(function(entry) { // Iterates over each entry - there may be multiple if batched
-      // Gets the message. entry.messaging is an array, but will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-      // Get the sender PSID
-      let sender_psid = webhook_event.sender.id;
-      console.log('Sender PSID: ' + sender_psid);
-      // Check if the event is a message or postback and pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);
-      } else if (webhook_event.postback) {
-        handlePostback(sender_psid, webhook_event.postback);
+      if(typeof(entry.messaging)!="undefined"){
+      // console.log(typeof(entry));
+        // Gets the message. entry.messaging is an array, but will only ever contain one message, so we get index 0
+        let webhook_event = entry.messaging[0];
+        // console.log(webhook_event);
+        // Get the sender PSID
+        let sender_psid = webhook_event.sender.id;
+        // console.log('Sender PSID: ' + sender_psid);
+        // Check if the event is a message or postback and pass the event to the appropriate handler function
+        if (webhook_event.message) {
+          handleMessage(sender_psid, webhook_event.message);
+        } else if (webhook_event.postback) {
+          handlePostback(sender_psid, webhook_event.postback);
+        }
+      }
+      if(typeof(entry.standby)!="undefined"){
+        console.log("Se recibio evento STANDBY");
       }
     });
     res.status(200).send('EVENT_RECEIVED'); // Returns a '200 OK' response to all requests
@@ -63,7 +69,29 @@ function handleMessage(sender_psid, received_message) {
     if (received_message.text) {
       // Create the payload for a basic text message
       response = {
-        "text": `Recibimos este mensaje: "${received_message.text}". ¡Ahora envía una imagen!`
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Bienvenido a la página\nSoy el Foto-bot",
+              "subtitle": "¿En qué te puedo ayudar?",
+              "image_url": "https://www.pngkit.com/png/detail/955-9556311_camara-de-fotos-caricatura.png",
+              "buttons": [
+                {
+                  "type": "postback",
+                  "title": "Información",
+                  "payload": "info",
+                },
+                {
+                  "type": "postback",
+                  "title": "Contactar a un humano",
+                  "payload": "handover",
+                }
+              ],
+            }]
+          }
+        }
       }
     }
     else if (received_message.attachments) {
@@ -103,10 +131,11 @@ function handlePostback(sender_psid, received_postback) {
   let response;
   let payload = received_postback.payload; //Get the payload for the postback
   // Set the response based on the postback payload
-  if (payload === 'yes') {
-    response = { "text": "Gracias!" }
-  } else if (payload === 'no') {
-    response = { "text": "Oops, Intenta enviar otra imagen." }
+  if (payload === 'info') {
+    response = { "text": "Una información Lorem Ipsum" }
+  } else if (payload === 'handover') {
+    response = { "text": "Lo estamos transfiriendo" }
+    callHandover(sender_psid);
   }
   callSendAPI(sender_psid, response); // Send the message to acknowledge the postback
 }
@@ -133,4 +162,23 @@ function callSendAPI(sender_psid, response) {
   });
   // console.log(process.env.PAGE_ACCESS_TOKEN);
   // console.log(JSON.stringify(request_body));
+}
+
+function callHandover(sender_psid){
+  let handover_req={
+    "recipient":{"id":sender_psid},
+    "target_app_id":263902037430900,
+    "metadata":"Se solicitó atención de una persona"}
+  request({
+    "uri": "https://graph.facebook.com/v8.0/107336410850663/pass_thread_control",
+    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": handover_req
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('handover ejecutado')
+    } else {
+      console.error("Error al enviar ejecutar handover:" + err);
+    }
+  });
 }
